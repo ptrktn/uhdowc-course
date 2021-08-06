c1b300f7d6624673b7f6d87ced3788653b4aa685
Natto <ptrktn@users.noreply.github.com>
2021-08-06 02:49:00 +0000

    Add part2 exercise 11

---
+++ part2/part2.11/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.ExtensionApp.open_browser = False
+# sauna
+c.NotebookApp.password = 'sha1:b84766345c0a:919e47d306a5dccf1b4647dfaff2a14e79b9ed8c'
+c.NotebookApp.password_required = True
